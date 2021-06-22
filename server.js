fd95c75cbe237ee27feeee66ae723f83bcb2020e
ygorverde <ygorverde572@gmail.com>
2021-06-22 00:10:32 -0300

    config express

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('TESTANDO')
+const express = require('express');
+const { resolve } = require('path');
+const app = express();
+
+app.use('/',
+    express.static(
+        resolve(
+            __dirname,
+            './build'
+        )
+    )
+)
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) { return console.log('Erro: ', err) }
+    console.log('Funcionado!')
+})
+
